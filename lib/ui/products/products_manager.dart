ae5097e6a2a06dfb1d33da81af788b0417f9c232
DuongThiYenNhiB1909961 <nhib1808@gmail.com>
2022-10-29 10:48:54 +0700

    Cap nhat, xoa san pham tren Realtime Database cua Firebase

--- lib/ui/products/products_manager.dart
+++ lib/ui/products/products_manager.dart
@@ -54,10 +54,11 @@ class ProductsManager with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  void updateProduct(Product product) {
+  Future<void> updateProduct(Product product) async {
     final index = _items.indexWhere((item) => item.id == product.id);
     if (index >= 0) {
-      _items[index] = product;
+      if (await _productsService.updateProduct(product))
+        _items[index] = product;
       notifyListeners();
     }
   }
@@ -67,9 +68,14 @@ class ProductsManager with ChangeNotifier {
     product.isFavorite = !savedStatus;
   }
 
-  void deleteProduct(String id) {
+  Future<void> deleteProduct(String id) async {
     final index = _items.indexWhere((item) => item.id == id);
+    Product? existingProduct = _items[index];
     _items.removeAt(index);
     notifyListeners();
+
+    if (await _productsService.deleteProduct(id))
+        _items.insert(index, existingProduct);
+      notifyListeners();
   }
 }
